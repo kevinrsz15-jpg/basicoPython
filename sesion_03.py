8af2aaf21cdd4e937b1ceb8e2573eeda5924ccf7
kevinrsz15 <kevinrsz15@gmail.com>
2026-02-12 17:36:17 -0800

    agregando

--- sesion_03.py
+++ sesion_03.py
@@ -1,4 +1,5 @@
 #Loop
+
 mi_lista = [1,2,3,4,5]
 
 for i in mi_lista:
@@ -18,3 +19,45 @@ mi_lista_2 = ["Lunes", "Martes","Miercoles", "Jueves", "Viernes"]
 for i in mi_lista_2:
     if i !="Lunes":
         print(f"Feliz{i}!")
+
+# While loop
+i = 0
+
+while 1 < 5:
+    i += 1
+    if i ==3:
+       continue
+    print(i)
+    if i ==4:
+        break
+
+else:
+    print("i es ahora mayor o igual a 5")
+
+#Prectica 2
+# Dada la lista mi_lista2 = ["lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+#Imprimir cada elemento de la lista 3 veces y cuando sea lunes no lo incluyas
+#Pista: Usa los dos tipos de loop while y for en el mismo programa para lograrlo
+#Resultado: 
+#martes
+#miercoles
+#jueves
+#Viernes
+#martes
+#miercoles
+#jueves
+#Viernes
+#martes
+#miercoles
+#jueves
+#
+print("=====RESULTADO DE INTENTO====")
+veces = 3
+limite = 4
+mi_lista_2 = ["Lunes", "Martes","Miercoles", "Jueves", "Viernes"]
+for veces in mi_lista_2:
+     if i !="Lunes":
+        print(f"Feliz{i}!")
+        break
+
+print("Ya se a generado")
