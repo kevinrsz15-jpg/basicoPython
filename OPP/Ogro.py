9c1640fd20b2d65a372fce4ea8b68474bd1d342b
kevinrsz15 <kevinrsz15@gmail.com>
2026-03-02 15:39:02 -0800

    Completando la informacion de juego

--- OPP/Ogro.py
+++ OPP/Ogro.py
@@ -1,10 +1,15 @@
 from Enemigo import *
-
-
+import random
 class Ogro(Enemigo):
     def _init_(self,puntos_energia=20,ataque=3):
         super()._init(tipo_enemigo='Ogro',puntos_energia=puntos_energia,ataque=ataque)
 
     def habla(self):
         print("Ogro aplastar todo!!!!")
-        #acc
+        
+    def ataque_especial(self):
+        print("Ogro ataque especial")
+        funcion_ataque_especial = random.random() < 0.20
+        if funcion_ataque_especial:
+            self.ataque += 4
+            print("Ogro enojado y incremento de ataque por 4")
